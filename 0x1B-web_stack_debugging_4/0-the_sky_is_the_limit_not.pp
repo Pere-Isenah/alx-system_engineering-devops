8195f25bd2e6d3e4ef67d42ff1f079d8e2babb70
Pere-Isenah <pereisenah@gmail.com>
2023-07-21 12:43:35 +0100

    task 1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,28 +1,11 @@
-# This Puppet manifest increases the amount of traffic an Nginx server can handle.
+# Increase the amount of traffic that nginx can take per time
 
-# Step 1: Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
+exec { 'increase-limit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Step 2: Restart Nginx after the configuration change
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Exec['fix--for-nginx'],
 }
 
-# This Puppet manifest allows the user 'holberton' to login and open files without errors.
-
-# Step 3: Increase the hard file limit for the Holberton user
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
-}
-
-# Step 4: Increase the soft file limit for the Holberton user
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
